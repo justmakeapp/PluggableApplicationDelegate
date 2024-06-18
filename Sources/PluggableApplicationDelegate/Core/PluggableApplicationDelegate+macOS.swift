3dbe2e6599be863205101a6b1d9f6216b4ed0435
Long Vu <long.vudai01@gmail.com>
2024-06-18 14:23:08 +0700

    run swifformat

--- Sources/PluggableApplicationDelegate/Core/PluggableApplicationDelegate+macOS.swift
+++ Sources/PluggableApplicationDelegate/Core/PluggableApplicationDelegate+macOS.swift
@@ -34,7 +34,6 @@
                         if finalResult == .terminateNow {
                             finalResult = .terminateLater
                         }
-
                     case .terminateNow:
                         break
                     @unknown default:
