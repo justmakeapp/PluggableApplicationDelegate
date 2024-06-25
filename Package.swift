b946934a322f31b6df2c13f0be1fc814edbb216e
Logan <long.vudai01@gmail.com>
2024-06-25 09:03:32 +0700

    add swiftSettings

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -15,12 +15,27 @@ let package = Package(
             targets: ["PluggableApplicationDelegate"]
         ),
     ],
-    dependencies: [
-    ],
     targets: [
         .target(
             name: "PluggableApplicationDelegate",
-            dependencies: []
+            swiftSettings: makeMigrateSwift6SwiftSettings()
         ),
     ]
 )
+
+private func makeMigrateSwift6SwiftSettings() -> [SwiftSetting] {
+    [
+        //        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_CONCISE_MAGIC_FILE"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_DEPRECATE_APPLICATION_MAIN"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_DISABLE_OUTWARD_ACTOR_ISOLATION"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_FORWARD_TRAILING_CLOSURES"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_IMPLICIT_OPEN_EXISTENTIALS"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_IMPORT_OBJC_FORWARD_DECLS"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_INFER_SENDABLE_FROM_CAPTURES"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_ISOLATED_DEFAULT_VALUES"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_GLOBAL_CONCURRENCY"),
+//        .enableUpcomingFeature("SWIFT_UPCOMING_FEATURE_REGION_BASED_ISOLATION"),
+//        .enableExperimentalFeature("StrictConcurrency=complete"),
+//        .swiftLanguageVersion(.v6)
+    ]
+}
